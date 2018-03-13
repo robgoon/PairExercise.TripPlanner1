043518242722c145ec8022181c902b6d81cc65c6
Beno Suriano <bjsuriano@gmail.com>
2018-03-13 14:06:07 -0600

    Added map.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('test');
+'strict only';
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYmVubzdzdXJpYW5vIiwiYSI6ImNqZXEyZGNsejU0amMycm83dDN5NDd0dGkifQ.As2dldVU8t2LPQJqh6NxtQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
